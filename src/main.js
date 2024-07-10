0079a1b52a96e965d2be1900f6f65a960f57279c
861108989 <861108989@qq.com>
2024-07-10 23:10:57 +0800

    构建项目

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// element plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+
+
+app.use(ElementPlus)
+
+app.mount('#app')
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // 开启本地局域网服务 host
+  server: {
+    host: '0.0.0.0',
+    open: true
+  }
+
+
+
+})
