4b3bb7b0cd6cd147bd0904f5e0b67d22a8c543ef
861108989 <861108989@qq.com>
2024-07-11 17:34:18 +0800

    侧边栏和搜索框ui

--- src/views/UserMessage/index.vue
+++ src/views/UserMessage/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="usermessage">
-    <div></div>
+    <div class="itembox">
+      <div class="avator left">
+        <el-icon size="50"><UserFilled /></el-icon>
+      </div>
+      <div class="right">
+        <div class="username">张三</div>
+        <div class="msg">消息</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style lang="scss" scoped>
